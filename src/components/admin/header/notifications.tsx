716ce512d1490d254ad2611f30c44cf3cd079e7a
Belvis <99735952+Belvis@users.noreply.github.com>
2024-02-01 20:13:47 +0700

    quick fix

--- src/components/admin/header/notifications.tsx
+++ src/components/admin/header/notifications.tsx
@@ -1,12 +1,21 @@
 import React, { useContext, useEffect, useState } from "react";
 
-import { BellOutlined } from "@ant-design/icons";
+import {
+  BellOutlined,
+  CheckOutlined,
+  MoreOutlined,
+  SelectOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 import {
   Badge,
   Button,
+  Col,
   Divider,
   Flex,
   Popover,
+  Row,
+  Segmented,
   Skeleton,
   Space,
   Spin,
@@ -14,7 +23,7 @@ import {
 } from "antd";
 import dayjs from "dayjs";
 
-const { Text } = Typography;
+const { Text, Title } = Typography;
 
 import { AxiosInstance } from "axios";
 import { stringify } from "query-string";
@@ -24,6 +33,7 @@ import { INotification } from "../../../interfaces";
 import { axiosInstance } from "../../../utils";
 import { CustomAvatar } from "./custom-avatar";
 import { NotificationMessage } from "./notifications-message";
+import styled from "styled-components";
 
 const httpClient: AxiosInstance = axiosInstance;
 
@@ -34,6 +44,7 @@ export const Notifications: React.FC = () => {
   const { mode } = useContext(ColorModeContext);
 
   const [open, setOpen] = useState(false);
+  const [moreOpen, setMoreOpen] = useState(false);
   const [notifications, setNotifications] = useState<INotification[]>([]);
   const [uneadCount, setUnreadCount] = useState<number>(0);
   const [totalCount, setTotalCount] = useState<number>(0);
@@ -127,59 +138,128 @@ export const Notifications: React.FC = () => {
     }
   }, [notifications]);
 
+  const MoreContent = styled(Row)`
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    widows: 100%;
+    padding: 5px;
+    border-radius: 5px;
+    &:hover {
+      background-color: #fff2e8;
+    }
+  `;
+
+  const moreContent = (
+    <div className="more-content" style={{ width: "100%" }}>
+      <MoreContent gutter={[16, 24]}>
+        <Col span={3}>
+          <CheckOutlined />
+        </Col>
+        <Col span={21}>
+          <Text strong>Đánh dấu là đã đọc</Text>
+        </Col>
+      </MoreContent>
+      <MoreContent gutter={[16, 24]}>
+        <Col span={3}>
+          <SettingOutlined />
+        </Col>
+        <Col span={21}>
+          <Text strong>Cài đặt thông báo</Text>
+        </Col>
+      </MoreContent>
+      <MoreContent gutter={[16, 24]}>
+        <Col span={3}>
+          <SelectOutlined />
+        </Col>
+        <Col span={21}>
+          <Text strong>Mở thông báo</Text>
+        </Col>
+      </MoreContent>
+    </div>
+  );
+
   const content = (
-    <Space
-      direction="vertical"
-      split={<Divider style={{ margin: 0 }} />}
-      style={{ width: "100%" }}
-    >
-      {notifications &&
-        notifications.length > 0 &&
-        notifications.map((noti, index) => (
-          <Link
-            to={noti.href}
-            onClick={() => {
-              readNotif(noti.id);
-              setOpen(false);
-            }}
-            key={index}
+    <React.Fragment>
+      <div className="noti-header" style={{ marginBottom: "1rem" }}>
+        <Row align="middle" justify="space-between">
+          <Text strong style={{ fontSize: "24px" }}>
+            Thông báo
+          </Text>
+          <Popover
+            placement="bottomRight"
+            content={moreContent}
+            trigger="click"
+            open={moreOpen}
+            onOpenChange={(newOpen) => setMoreOpen(newOpen)}
+            overlayStyle={{ width: 250 }}
           >
-            <Skeleton
-              avatar
-              title={false}
-              loading={!noti.id}
-              active
+            <Button
+              shape="circle"
+              icon={<MoreOutlined />}
+              style={{
+                border: 0,
+                backgroundColor: mode === "light" ? "#F7F8F9" : undefined,
+              }}
+            />
+          </Popover>
+        </Row>
+        <Row style={{ marginTop: "0.25rem" }}>
+          <Segmented options={["Tất cả", "Chưa đọc"]} />
+        </Row>
+      </div>
+      <Space
+        direction="vertical"
+        split={<Divider style={{ margin: 0 }} />}
+        style={{ width: "100%" }}
+      >
+        {notifications &&
+          notifications.length > 0 &&
+          notifications.map((noti, index) => (
+            <Link
+              to={noti.href}
+              onClick={() => {
+                readNotif(noti.id);
+                setOpen(false);
+              }}
               key={index}
             >
-              <Flex justify="space-between" align="center" key={index}>
-                <Space key={index}>
-                  <CustomAvatar
-                    size={48}
-                    shape="square"
-                    src={
-                      noti.customer
-                        ? noti.customer.image
-                        : "https://res.cloudinary.com/dwoxggxq7/image/upload/w_400,h_400,c_scale/su1bf6xpowuscnuuerwm.jpg"
-                    }
-                    name={noti.content}
-                  />
-                  <Space direction="vertical" size={0}>
-                    <NotificationMessage
-                      content={noti.content}
-                      type={noti.notificationType}
+              <Skeleton
+                avatar
+                title={false}
+                loading={!noti.id}
+                active
+                key={index}
+              >
+                <Flex justify="space-between" align="center" key={index}>
+                  <Space key={index}>
+                    <CustomAvatar
+                      size={48}
+                      shape="square"
+                      src={
+                        noti.customer
+                          ? noti.customer.image
+                          : "https://res.cloudinary.com/dwoxggxq7/image/upload/w_400,h_400,c_scale/su1bf6xpowuscnuuerwm.jpg"
+                      }
+                      name={noti.content}
                     />
-                    <Text type="secondary">
-                      {dayjs(new Date(noti.createdAt)).fromNow()}
-                    </Text>
+                    <Space direction="vertical" size={0}>
+                      <NotificationMessage
+                        content={noti.content}
+                        type={noti.notificationType}
+                      />
+                      <Text type="secondary">
+                        {dayjs(new Date(noti.createdAt)).fromNow()}
+                      </Text>
+                    </Space>
                   </Space>
-                </Space>
-                <div>{!noti.read && <Badge status="processing" />}</div>
-              </Flex>
-            </Skeleton>
-          </Link>
-        ))}
-      {loadMore}
-    </Space>
+                  <div>{!noti.read && <Badge status="processing" />}</div>
+                </Flex>
+              </Skeleton>
+            </Link>
+          ))}
+        {loadMore}
+      </Space>
+    </React.Fragment>
   );
 
   const loadingContent = (
