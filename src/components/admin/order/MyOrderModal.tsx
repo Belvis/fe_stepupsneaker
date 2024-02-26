620b98382b0a3e0bce56d3c305b40b955e2699f5
LeVan-Duy <121452628+LeVan-Duy@users.noreply.github.com>
2024-02-26 14:28:07 +0700

    update order admin 26/2

--- src/components/admin/order/MyOrderModal.tsx
+++ src/components/admin/order/MyOrderModal.tsx
@@ -330,10 +330,9 @@ const MyOrderModal: React.FC<MyOrderModalProps> = ({
   };
 
   const handleUpdateOrder = () => {
-    const simplifiedCartItems: { id: string; quantity: number }[] =
-      viewOrder.orderDetails.map((item) => {
-        return { id: item.productDetail.id, quantity: item.quantity };
-      });
+    const simplifiedCartItems: { id: string; quantity: number }[] = viewOrder.orderDetails.map((item) => {
+      return { id: item.id, quantity: item.quantity };
+    });
     const orderPayload = orderToPayload(order);
 
     const submitData = {
